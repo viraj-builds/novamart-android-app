ec2f6bc447adee8a903c9c776b5771d86a0b2d13
Viraj Walavalkar <viraj.walavalkar@clevertap.com>
2026-08-05 12:18:43 +0530

    Add initial Windows support and project configuration
    - Created CMakeLists.txt for Windows platform.
    - Added Flutter window management and resource files.
    - Implemented basic window functionality with DPI awareness.
    - Included necessary Flutter plugin registration for Firebase and permission handling.
    - Added application icons and manifest for Windows.
    - Established console output redirection for debugging.
    - Integrated Flutter engine with the Windows application.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_analytics
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import shared_preferences_foundation
+import sqflite_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
